521859ba04dbb348d05504c8a584f15ceae0fcc7
Alejandro Cedillo Martin <cedillito7@gmail.com>
2025-03-10 21:04:26 +0000

    changed the movement for it to be 2D top down

--- Assets/Scenes/Player.cs
+++ Assets/Scenes/Player.cs
@@ -6,6 +6,7 @@ public class Player : MonoBehaviour
     private Rigidbody2D rb;
 
     [SerializeField]private float moveSpeed;
+    [SerializeField] private float SpeedX, SpeedY;
     [SerializeField]private float jumpForce;
 
     private float xInput;
@@ -20,13 +21,8 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        xInput = Input.GetAxisRaw("Horizontal");
-
-        rb.linearVelocity = new Vector2(xInput * moveSpeed, rb.linearVelocity.y); 
-
-        if(Input.GetKeyDown(KeyCode.Space))
-        {
-            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
-        }   
+        SpeedX = Input.GetAxis("Horizontal") * moveSpeed;
+        SpeedY = Input.GetAxis("Vertical") * moveSpeed;
+        rb.linearVelocity = new Vector2(SpeedX, SpeedY);
     }
 }
