b1fe6c0b75c22955cfb33ac6bdba7bbee4898619
Eric Arends <89783401+eric-arends@users.noreply.github.com>
2025-03-11 11:29:18 +0100

    Added aiming.

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -7,24 +7,26 @@ public class PlayerController : MonoBehaviour
     
     // Movespeed. Editable in the editor directly for prototyping
     public float moveSpeed = 5f;
+    public float aimMoveSpeedMultiplier = 0.5f;
+    public bool isAiming = false;
     private Vector2 moveInput;
     private Rigidbody2D rb;
-
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
     }
-    
-    // Gets Move direction from the Input System
     public void OnMove(InputValue cc)
     {
         moveInput = cc.Get<Vector2>();
     }
-
-
-    // Directly applies Move Direction to the RigidBody.
+    public void OnAim(InputValue cc)
+    {
+        isAiming = cc.Get<float>() > 0;
+        Debug.Log(cc.Get<float>());
+    }
+    // Directly applies Move Direction to the RigidBody's velocity.
     private void FixedUpdate()
     {
-        rb.linearVelocity = moveInput * moveSpeed;
+        rb.linearVelocity = moveInput * (isAiming ? moveSpeed * 0.5f : moveSpeed);
     }
 }
